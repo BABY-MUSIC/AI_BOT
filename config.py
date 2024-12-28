a62d2779614548359f9605572101faf570c0de02
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-12-29 01:40:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL", "https://api.together.xyz/v1/chat/completions")
 
-SUPPORT_LINK = os.getenv("SUPPORT_LINK", "https://t.me/BABY09_WORLD")
-UPDATES_LINK = os.getenv("UPDATES_LINK", "https://t.me/BABY09_UPDATES")
+SUPPORT_LINK = os.getenv("SUPPORT_LINK", "https://t.me/+OL6jdTL7JAJjYzVl")
+UPDATES_LINK = os.getenv("UPDATES_LINK", "https://t.me/BABY09_WORLD")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "")
